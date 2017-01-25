09592dd360dd628b2c438a156aecd54329d2ec7b
James Acklin <jamesacklin@gmail.com>
2017-01-25 15:38:16 -0500

    Styling for testimonials

--- components/features/testimonials/content-testimonials.php
+++ components/features/testimonials/content-testimonials.php
@@ -5,17 +5,11 @@
  * @package Apps4Clubz
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div <?php post_class(); ?>>
 
-	<div class="entry-content">
+	<blockquote>
 		<?php the_content(); ?>
-	</div>
+		<?php the_title( '<cite>&mdash;', '</cite>' ); ?>
+	</blockquote>
 
-	<?php the_title( '<header class="entry-header"><h2 class="entry-title">', '</h2></header>' ); ?>
-
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="post-thumbnail"><?php the_post_thumbnail( 'clubpress-thumbnail-avatar' ); ?></div>
-	<?php endif; ?>
-
-	<?php edit_post_link( esc_html__( 'Edit', 'clubpress' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-</article>
+</div>
