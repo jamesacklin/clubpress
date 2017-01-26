9c913a0e9afd45e62c41d489a4f5ccd26064f15f
James Acklin <jamesacklin@gmail.com>
2017-01-26 11:52:03 -0500

    Homepage feature grid in place

--- front-page.php
+++ front-page.php
@@ -20,6 +20,23 @@ get_header(); ?>
 				<?php get_template_part( 'components/features/hero-image/content', 'hero' ); ?>
 			<?php endwhile; ?>
 
+			<?php if ( have_rows( 'homepage_features' ) ) : ?>
+				<div class="homepage-features">
+					<?php while ( have_rows( 'homepage_features' ) ) : the_row(); ?>
+						<div class="homepage-feature">
+							<?php if ( get_sub_field( 'feature_illustration') ) { ?>
+								<img src="<?php the_sub_field( 'feature_illustration' ); ?>" />
+							<?php } ?>
+							<h2><?php the_sub_field( 'feature_name' ); ?></h2>
+							<h3><?php the_sub_field( 'feature_subhead' ); ?></h3>
+							<p><?php the_sub_field( 'feature_text' ); ?></p>
+						</div>
+					<?php endwhile; ?>
+				</div>
+			<?php else : ?>
+				<?php // no rows found ?>
+			<?php endif; ?>
+
 			<?php get_template_part( 'components/features/testimonials/testimonials' ); ?>
 		</main>
 	</div>
