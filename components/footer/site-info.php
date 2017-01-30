835b83f8b5c01f31c80dccb961224bf5403d1ba2
James Acklin <jamesacklin@gmail.com>
2017-01-30 12:46:59 -0500

    Some styling changes

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,3 @@
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clubpress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'clubpress' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'clubpress' ), 'Apps4Clubz', '<a href="http://www.jamesacklin.com/" rel="designer">James Acklin</a>' ); ?>
+	Apps4Clubz is a product of Stabertson Technologies, LLC. ©2016 All rights reserved. &bull; <a href="#">Privacy Policy</a>
 </div><!-- .site-info -->
